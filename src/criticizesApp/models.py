902d44a775a9ae2edae0f1773da7db63699587d8
Cedric Morgantini <c.morgantini@free.fr>
2021-09-16 19:00:17 +0200

    Ajout 1ere migration, script models, modification urls.py et requirements.txt

--- src/criticizesApp/models.py
+++ src/criticizesApp/models.py
@@ -5,7 +5,14 @@ from django.db import models
 
 class Ticket(models.Model):
     # Your Ticket model definition goes here
-    pass
+    title = models.CharField(max_length=128)
+    description = models.TextField(max_length=2048, blank=True)
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    time_created = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
 
 
 class Review(models.Model):
@@ -22,6 +29,10 @@ class Review(models.Model):
 
 class UserFollows(models.Model):
     # Your UserFollows model definition goes here
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+                             related_name='following')
+    followed_user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+                                      related_name='followed_by')
 
     class Meta:
         # ensures we don't get multiple UserFollows instances
